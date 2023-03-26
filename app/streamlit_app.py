4c34c77a6e45a04bc4bf3eabca7226c993d3cd51
ru35hub <jonas.spaeth@campus.lmu.de>
2023-03-26 19:30:50 +0200

    update landing page

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -13,13 +13,19 @@ import base64
 
 # Test: Linechart from a dataframe
 
-st.write("Here's our first attempt at using data to create a table: Tada")
 
+st.header("Druckdiagramme Bayern")
+st.subheader(":dash: :blue[Föhn] über den Alpen  :dash:  Nord-/ Südpassat an :blue[Walchensee & Kochelsee]  :dash:  Peler/ Ora am :blue[Gardasee]")
+
+st.write("Passwort eingeben um Diagramme zu sehen.")
 ENCODED_PW = b"aXNpdHdpbmR5"
 pw_input = st.text_input(label="password", type="password")
 decoded_pw = base64.b64decode(ENCODED_PW).decode()
 
 if decoded_pw == pw_input:
     for diagram in PDiffDiagrams:
+
         fig, ax = run(diagram)
         st.pyplot(fig)
+
+        st.markdown("---")
