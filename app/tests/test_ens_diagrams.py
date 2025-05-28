f1f70850981665f2820b06213f19c3c9eb2b3bb0
Jonas Spaeth <jonny.mail@gmx.de>
2025-05-28 22:28:43 +0200

    update tests and swith from unittest to pytest

--- app/tests/test_ens_diagrams.py
+++ app/tests/test_ens_diagrams.py
@@ -1,82 +1,77 @@
-from unittest import TestCase
-
 import numpy as np
 import pandas as pd
+import pytest
 
-from ..sources.data_retreive.km_forecast import get_pressure, get_ensemble_icond2
+from ..sources.data_retreive.km_forecast import get_pressure
 from ..sources.data_plotting import run, plot_p_diff
 from ..sources.PDiffDiagrams import PDiffDiagrams
+from ..sources.diagram_classes import DiagramWalchensee
+
 import matplotlib.pyplot as plt
 import matplotlib
-from ..sources.diagram_classes import DiagramWalchensee
+matplotlib.use("MacOSX")  # Set backend once globally
 
 import hvplot.pandas  # Import the hvplot.pandas extension
 import holoviews as hv
-from bokeh.plotting import show
 from holoviews import opts
+from bokeh.plotting import show
 from bokeh.models.formatters import DatetimeTickFormatter
 
 
-class TestEnsDiagram(TestCase):
-    matplotlib.use("MacOSX")
+def test_diagram():
+    """Test rendering of the Walchensee diagram."""
+    diagram = DiagramWalchensee()
+    plot = diagram.plot()
+    show(hv.render(plot))
 
-    def test_diagram(self):
-        walchenseeDiagram = DiagramWalchensee()
-        plot = walchenseeDiagram.plot()
-        show(hv.render(plot))
 
-    def test_try_plot(self):
-        # Create a datetime index spanning 2.5 days with 1-hour intervals
-        date_rng = pd.date_range(
-            start="2023-10-30 09:00:00", end="2023-11-01 21:30:00", freq="H"
-        )
-        # Create random sample data for the DataFrame
-        np.random.seed(42)
-        data = np.random.normal(size=(len(date_rng), 21))
-        data = np.cumsum(data, axis=0)
-        # Create the Pandas DataFrame
-        pdiff = pd.DataFrame(data, index=date_rng, columns=range(21))
+def test_try_plot():
+    """Test ensemble member and mean plotting with synthetic data."""
+    date_rng = pd.date_range(
+        start="2023-10-30 09:00:00", end="2023-11-01 21:30:00", freq="H"
+    )
+    np.random.seed(42)
+    data = np.random.normal(size=(len(date_rng), 21))
+    data = np.cumsum(data, axis=0)
+    pdiff = pd.DataFrame(data, index=date_rng, columns=range(21))
 
-        formatter = DatetimeTickFormatter(
-            hours="%Hh", days="%a, %d. %b"
-        )  # , months='%b %Y')
+    formatter = DatetimeTickFormatter(hours="%Hh", days="%a, %d. %b")
 
-        member_plot = pdiff.hvplot.line(
-            title="Interactive Line Plot",
-            color="lightblue",
-            line_width=1,
+    member_plot = pdiff.hvplot.line(
+        title="Interactive Line Plot",
+        color="lightblue",
+        line_width=1,
+        tools=[],
+        legend=False,
+    )
+
+    ensmean_plot = (
+        pdiff.mean(axis=1)
+        .rename("Ensemble mean")
+        .hvplot.line(
+            title="Pressure diagram Walchensee/ Kochelsee (+: Foehn, -: Thermal winds)",
+            color="darkblue",
+            line_width=3,
             tools=[],
+            grid=True,
             legend=False,
         )
+    )
 
-        # ensemble mean
-        ensmean_plot = (
-            pdiff.mean(axis=1)
-            .rename("Ensemble mean")
-            .hvplot.line(
-                title="Pressure diagram Walchensee/ Kochelsee (+: Foehn, -: Thermal winds)",
-                color="darkblue",
-                line_width=3,
-                tools=[],
-                grid=True,
-                legend=False,
-            )
+    hline_threshold_kws = dict(line_width=1, color="gray", line_dash="dashed")
+    hline_zero_kws = dict(line_width=1.5, color="black")
+    hlines = (
+        hv.HLine(-2).opts(**hline_threshold_kws)
+        * hv.HLine(2).opts(**hline_threshold_kws)
+        * hv.HLine(0).opts(**hline_zero_kws)
+    )
+
+    combined_plot = member_plot * ensmean_plot * hlines
+
+    combined_plot.opts(
+        opts.Curve(
+            xrotation=45, ylabel="hPa", xformatter=formatter, tools=["hover"]
         )
+    )
 
-        hline_threshold_kws = dict(line_width=1, color="gray", line_dash="dashed")
-        hline_zero_kws = dict(line_width=1.5, color="black")
-        hlines = (
-            hv.HLine(-2).opts(**hline_threshold_kws)
-            * hv.HLine(2).opts(**hline_threshold_kws)
-            * hv.HLine(0).opts(**hline_zero_kws)
-        )
-
-        combined_plot = member_plot * ensmean_plot * hlines
-
-        combined_plot.opts(
-            opts.Curve(
-                xrotation=45, ylabel="hPa", xformatter=formatter, tools=["hover"]
-            )
-        )
-
-        show(hv.render(combined_plot))
+    show(hv.render(combined_plot))
--- app/tests/test_km_forecast.py
+++ app/tests/test_km_forecast.py
@@ -1,33 +1,49 @@
-from unittest import TestCase
-from ..sources.data_retreive.km_forecast import get_pressure, get_ensemble_icond2
-from ..sources.data_plotting import run, plot_p_diff
-from ..sources.PDiffDiagrams import PDiffDiagrams
 import matplotlib.pyplot as plt
 import matplotlib
+matplotlib.use("MacOSX")
+
+import pytest
+
+from ..sources.data_retreive.km_forecast import get_pressure
+from ..sources.data_retreive.data_retreiver import parse_ens_page, download_ens_page
+from ..sources.data_retreive.utils import city_codes
+from ..sources.data_plotting import run, plot_p_diff
+from ..sources.PDiffDiagrams import PDiffDiagrams
 
 
-class Test(TestCase):
-    matplotlib.use("MacOSX")
 
-    def test_get_pressure(self):
-        # for model in ["sui-hd", "rapid-id2", "rapid-euro", "usa"]:
-        #     print(model)
-        #     fig, ax = plt.subplots(1, 1, figsize=(8, 4))
-        #     plot_p_diff("innsbruck", 'brescia', model=model, ax=ax, horiz_lines=2)
-        #     plt.show()
-
-        # test = run(PDiffDiagrams.foehn)
-        # test = run(PDiffDiagrams.walchensee)
-        fig, ax = run(PDiffDiagrams.walchensee)
+def test_get_pressure():
+    """Manually inspect pressure plots for known locations and diagrams."""
+    for diagram in [PDiffDiagrams.walchensee, PDiffDiagrams.foehn, PDiffDiagrams.gardasee]:
+        fig, ax = run(diagram)
         plt.show()
-        fig, ax = run(PDiffDiagrams.foehn)
-        plt.show()
-        fig, ax = run(PDiffDiagrams.gardasee)
-        plt.show()
-        # response = get_pressure("innsbruck", 'ecmw')
-        # print(response)
 
-    def test_get_ensemble_icond2(self):
-        df = get_ensemble_icond2()
-        df.plot()
-        plt.show()
+    # If you want to test return structure instead of plotting, use:
+    # response = get_pressure("innsbruck", "ecmw")
+    # assert isinstance(response, pd.DataFrame) or similar
+
+
+# def test_get_ensemble_icond2():
+#     """Test ensemble retrieval and plotting."""
+#     df = get_ensemble_icond2()
+#     assert df is not None
+#     assert not df.empty
+#     df.plot()
+#     plt.show()
+
+
+# def test_download_ens_page():
+#     city1 = "innsbruck"
+#     page_city1 = download_ens_page(
+#         location=city_codes[self.city1], model="rapid-id2", variable="luftdruck"
+#     )
+    
+
+
+def test_parse_ens_page():
+    city = "starnberg"
+    page = download_ens_page(
+        location=city_codes[city], model="rapid-id2", variable="luftdruck"
+    )
+    data = parse_ens_page(page)
+    print(data)
